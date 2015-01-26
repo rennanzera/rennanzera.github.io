5bd4aeb0fe554ca347199783d11c290d6f431229
Rennan <rennanslt@gmail.com>
2015-01-26 14:52:10 -0200

    Grunt things

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      build: {
+        src: 'public/js/fluidvids.min.js',
+        dest: 'public/js/build/global.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
