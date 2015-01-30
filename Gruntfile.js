92b6d2827770d766e198778e86ffa93456216118
Rennan <rennanslt@gmail.com>
2015-01-30 11:48:54 -0200

    Setup css min

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,13 +10,23 @@ module.exports = function(grunt) {
         src: 'public/js/fluidvids.js',
         dest: 'public/js/build/global.min.js'
       }
-    }
-  });
+    },
+    cssmin: {
+      build: {
+          src: ['public/css/poole.css', 'public/css/syntax.css'],
+          dest: 'public/css/build/global.min.css'
+      }
+  }
+});
+
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load the plugin that provides the "cssmin" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('buildcss',  ['cssmin']);
 
 };
